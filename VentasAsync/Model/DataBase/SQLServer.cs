0b863331889b9548f681caade77f80a1f923d572
Cristo Vazquez <c.j_13_10@hotmail.com>
2025-05-24 10:53:20 -0700

    Add project files.

---
+++ VentasAsync/Model/DataBase/SQLServer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using VentasAsync.Model.Entities;
+
+namespace VentasAsync.Model.DataBase
+{
+    internal class SQLServer
+    {
+        private readonly string _connectionString;
+        public SQLServer(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Task<T> ReaderAsync<T>(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                
+            }
+        }
+    }
+}
