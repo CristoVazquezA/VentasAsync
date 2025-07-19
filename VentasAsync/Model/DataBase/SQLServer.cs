a1ec15b38cc0126363e86f6736b4a91a0d45248f
Cristo Vazquez <c.j_13_10@hotmail.com>
2025-07-19 12:50:45 -0700

    Agregando metodos transaccionales

--- VentasAsync/Model/DataBase/SQLServer.cs
+++ VentasAsync/Model/DataBase/SQLServer.cs
@@ -12,7 +12,6 @@ namespace VentasAsync.Model.DataBase
             SQLServerConfiguration.GetConnectionString();
             _connectionString = SQLServerConfiguration.ConnectionString;
         }
-
         public SqlConnection GetConnection()
         {
             SqlConnection sqlConnection = new(_connectionString);
